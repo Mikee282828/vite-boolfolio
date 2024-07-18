9555a1a3ef3e3d8d02515de34ab1e9f386126eb7
MikeMac <micheledong2803@gmail.com>
2024-07-18 18:16:48 +0200

    card

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 export default {
   name: 'App',
   data() {
     return {
-
+      projects : ''
     }
   },
+  components:{
+    ProjectCard
+  },
   mounted() {
-
+    axios
+    .get('http://localhost:8000/api/projects')
+    .then(
+      result=>{
+        console.log(result.data.projects.data);
+        this.projects = result.data.projects.data;
+      }
+    )
   }
 }
 </script>
 
 <template>
-  <div>
-    hi
+  <div class="container">
+    <div class="row">
+      <div class="col-4 my_height g-3" v-for="project in projects">
+        <ProjectCard :project="project"/>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.my_height{
+  height:20rem;
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'ProjectCard',
+
+    props: {
+        project: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <img v-if="!project.img_preview.startsWith('http')" :src="'http://localhost:8000/storage/' + project.img_preview" class="card-img-top h-100" alt="...">
+
+        <img v-else :src="project.img_preview" class="card-img-top object-fit-cover h-100" alt="...">
+
+        <div class="card-body overflow-auto">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">
+                {{ project.description }}
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
