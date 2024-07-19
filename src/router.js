6caa89cd6ff103581c86dd55bf6b89a9670df1dd
MikeMac <micheledong2803@gmail.com>
2024-07-19 12:10:04 +0200

    added routes and components

--- src/router.js
+++ src/router.js
@@ -1,17 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: 'AppHome'
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: 'AppAbout'
-    }
-  ]
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: 'AppHome'
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: 'AppAbout'
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: 'AppProjects'
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: 'AppContacts'
+        }
+    ]
 })
 export { router };
